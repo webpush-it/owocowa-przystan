e046e479204b2d7c7881cfeb059e35077e3a0f12
Konrad Beśka <konrad.beska@firma.interia.pl>
2026-02-05 23:08:35 +0100

    Vercel config for deploy

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 import sanity from '@sanity/astro'
+import vercel from '@astrojs/vercel'
 
 export default defineConfig({
+    // tryb 'hybrid': wszystkie odsłony są statyczne, chyba że dodasz `export const prerender = false`
+    output: 'hybrid',
+    adapter: vercel(),
     integrations: [
         tailwind(),
         react(),
