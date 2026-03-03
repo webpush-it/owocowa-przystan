5cec88e761d8aa772c2492d39d2e59c6fb72bb55
Konrad Beśka <konrad.beska@firma.interia.pl>
2026-03-03 17:30:29 +0100

    sitemap

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -6,6 +6,7 @@ import vercel from '@astrojs/vercel'
 import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
+    site: 'https://owocowaprzystan.pl',
     adapter: vercel(),
     integrations: [
         tailwind(),
