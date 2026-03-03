cb4ba200836944dac8a0990d012e239f4879b055
Konrad Beśka <konrad.beska@firma.interia.pl>
2026-03-03 17:24:38 +0100

    sitemap, robots.txt

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -3,12 +3,14 @@ import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 import sanity from '@sanity/astro'
 import vercel from '@astrojs/vercel'
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
     adapter: vercel(),
     integrations: [
         tailwind(),
         react(),
+        sitemap(),
         sanity({
             projectId: 'zcxx0aah',
             dataset: 'production',
